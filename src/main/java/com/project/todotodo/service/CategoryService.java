023d82dc35f254caf8751bb4c91053db53b19a7f
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-25 18:16:22 +0900

    category delete show register

--- src/main/java/com/project/todotodo/service/CategoryService.java
+++ src/main/java/com/project/todotodo/service/CategoryService.java
@@ -1,9 +1,10 @@
 package com.project.todotodo.service;
 
 import com.project.todotodo.domain.CategoryDomain;
+import com.project.todotodo.domain.NodeDomain;
 import com.project.todotodo.dto.Goal.GoalForm;
-import com.project.todotodo.model.Category;
 import com.project.todotodo.repository.CategoryRepository;
+import com.project.todotodo.repository.NodeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final NodeRepository nodeRepository;
 
-    public List<CategoryDomain> getCategorys(String name) {
-        List<CategoryDomain> categorys = categoryRepository.findAll();
-        return categorys;
+    public List<CategoryDomain> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public GoalForm createCategory(String name) {
+        CategoryDomain newCategory = new CategoryDomain();
+        NodeDomain node = new NodeDomain();
+
+        node.setContent(name);
+        newCategory.setNode(node);
+
+        CategoryDomain savedCategory = categoryRepository.save(newCategory);
+
+        GoalForm goalForm = new GoalForm();
+        goalForm.setName(savedCategory.getNode().getContent());
+
+        return goalForm;
+    }
+
+    public void deleteCategoryById(Long id) {
+        categoryRepository.deleteById(id);
     }
 }
