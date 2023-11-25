8397661157372d1ebaf44d681f01ca812b9d9946
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-25 17:46:38 +0900

    category all show

--- src/main/java/com/project/todotodo/service/CategoryService.java
+++ src/main/java/com/project/todotodo/service/CategoryService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public List<CategoryDomain> getCategorys() {
+    public List<CategoryDomain> getCategorys(String name) {
         List<CategoryDomain> categorys = categoryRepository.findAll();
         return categorys;
     }
