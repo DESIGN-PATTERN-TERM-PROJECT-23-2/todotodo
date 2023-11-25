ba9b4990f533ec2ccda5edc11af6db167751709f
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-25 17:42:39 +0900

    category all show

--- src/main/java/com/project/todotodo/service/CategoryService.java
+++ src/main/java/com/project/todotodo/service/CategoryService.java
@@ -1,12 +1,21 @@
 package com.project.todotodo.service;
 
+import com.project.todotodo.domain.CategoryDomain;
+import com.project.todotodo.dto.Goal.GoalForm;
+import com.project.todotodo.model.Category;
 import com.project.todotodo.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
+    public List<CategoryDomain> getCategorys() {
+        List<CategoryDomain> categorys = categoryRepository.findAll();
+        return categorys;
+    }
 }
