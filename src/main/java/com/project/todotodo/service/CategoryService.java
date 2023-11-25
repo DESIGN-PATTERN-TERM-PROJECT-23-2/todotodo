73410f5f116e110949c79ed24ee1703d0fdec16a
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-25 17:15:22 +0900

    conflict

--- src/main/java/com/project/todotodo/service/CategoryService.java
+++ src/main/java/com/project/todotodo/service/CategoryService.java
@@ -1,33 +1,12 @@
 package com.project.todotodo.service;
 
-import com.project.todotodo.dto.CategoryCreateRes;
-import com.project.todotodo.model.Category;
 import com.project.todotodo.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public List<Category> getCategorys(String name) {
-        List<Category> categorys = categoryRepository.findByname(name);
-        return getCategorys(name);
-    }
-
-    public CategoryCreateRes createCategory(String name){
-        Category newCategory = new Category();
-        CategoryCreateRes categoryCreateRes = new CategoryCreateRes();
-
-        newCategory.setName(name);
-
-        return categoryCreateRes;
-    }
-
-    public void deleteCategory(String name) {
-        categoryRepository.deleteByName(name);
-    }
 }
