af14522522eae7635a44313d59cbb426f110f80b
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-11-25 17:45:56 +0900

    goal delete, list api 추가

--- src/main/java/com/project/todotodo/controller/GoalController.java
+++ src/main/java/com/project/todotodo/controller/GoalController.java
@@ -1,13 +1,14 @@
 package com.project.todotodo.controller;
 
+import org.springframework.ui.Model;
+import com.project.todotodo.domain.CategoryDomain;
 import com.project.todotodo.dto.Goal.GoalForm;
 import com.project.todotodo.model.Category;
 import com.project.todotodo.service.CategoryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/goal", produces = "application/json;charset=utf8")
@@ -31,6 +32,21 @@ public class GoalController {
         System.out.println(goalForm.getName());
         return "redirect:/";
     }
+
+    @GetMapping("/list")
+    public String list(Model model){
+        //List<CategoryDomain> categories = categoryService.getAllCategories();
+        //model.addAttribute("categories", categories);
+        return "goal/list";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable Long id) {
+        // categoryService.deleteCategoryById(id);
+        return "redirect:/goal/list"; // 삭제 후 카테고리 목록으로 리다이렉트
+    }
+
+
 
 
 }
