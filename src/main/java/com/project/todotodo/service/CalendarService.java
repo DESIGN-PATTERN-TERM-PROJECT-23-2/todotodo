78d6c1480bdb5e40013b1bf40428ab8f5a09dc01
hyuun01 <hyuun1501@gmail.com>
2023-11-25 19:53:53 +0900

    calendar service -ing

--- src/main/java/com/project/todotodo/service/CalendarService.java
+++ src/main/java/com/project/todotodo/service/CalendarService.java
@@ -1,5 +1,6 @@
 package com.project.todotodo.service;
 
+import com.project.todotodo.dto.Calendar.OnlyCalendarInfo;
 import com.project.todotodo.repository.NodeRepository;
 import com.project.todotodo.repository.TodoListRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,10 @@ import org.springframework.stereotype.Service;
 public class CalendarService {
     private final TodoListRepository todoListRepository;
     private final NodeRepository nodeRepository;
+
+    public OnlyCalendarInfo getOnlyCalendarInfo(){
+        OnlyCalendarInfo onlyCalendarInfo = new OnlyCalendarInfo();
+
+        return onlyCalendarInfo;
+    }
 }
