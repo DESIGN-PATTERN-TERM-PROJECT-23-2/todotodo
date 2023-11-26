acb72f8a833fc614d78d37f24190d05583a8b541
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-11-26 23:19:01 +0900

    Service

--- src/main/java/com/project/todotodo/model/NodeList.java
+++ src/main/java/com/project/todotodo/model/NodeList.java
@@ -14,11 +14,19 @@ public class NodeList {
     NodeListIterator nodeListIterator;
 
     private Node parent;
+    private Node curr;
 
     private ArrayList<Node> children;
 
+    public NodeList(Node curr, Node parent){
+        this.curr = curr;
+        this.parent = parent;
+        this.children = new ArrayList<>();
+    }
+
     public NodeListIterator createIterator(){
-        return null;
+        this.nodeListIterator = new NodeListIterator(this, curr);
+        return this.nodeListIterator;
     }
 
 
