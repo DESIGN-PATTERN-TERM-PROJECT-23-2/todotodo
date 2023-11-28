0e443a5598fce91c4b4da44c563be6a16d4682af
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-11-28 21:41:13 +0900

    holub 연결

---
+++ src/main/java/com/holub/test/TodoConnectTest.java
@@ -0,0 +1,50 @@
+package com.holub.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.holub.database.*;
+import com.holub.text.ParseFailure;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TodoConnectTest {
+
+
+    Table name;
+    Table address;
+    Database database;
+
+    private void insertData() throws IOException {
+
+        Reader in_name = new FileReader("todo_lists.csv");
+        CSVImporter csvImporter = new CSVImporter(in_name);
+        name = TableFactory.create(csvImporter);
+
+        Reader in_address = new FileReader("nodes.csv");
+        CSVImporter csvImporter_address = new CSVImporter(in_address);
+        address = TableFactory.create(csvImporter_address);
+
+        database = new Database(new File("."));
+    }
+
+    @DisplayName("Select Test")
+    @Test
+    public void join_test() throws IOException, ParseFailure {
+        insertData();
+        Table join_table = database.execute("SELECT * FROM nodes, todo_lists WHERE nodes.node_id = todo_lists.node_id AND nodes.node_id = 2");
+        Table expectedTable = TableFactory.create("<anonymous>", new String[]{"addrId", "street", "city", "state", "zip", "first", "last", "addrId"});
+        expectedTable.insert(new Object[]{"0", "12 MyStreet", "Berkeley", "CA", "99998", "Allen", "Holub", 0});
+        expectedTable.insert(new Object[]{"1", "34 Quarry Ln.", "Bedrock", "AZ", "00000", "Fred", "Flintstone", 1});
+        expectedTable.insert(new Object[]{"1", "34 Quarry Ln.", "Bedrock", "AZ", "00000", "Wilma", "Flintstone", 1});
+
+        String string_expected = expectedTable.toString();
+
+        String string_join_table = join_table.toString();
+
+        Assertions.assertEquals(string_expected, string_join_table);
+    }
+}
