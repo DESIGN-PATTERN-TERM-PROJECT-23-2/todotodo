f9dbdef8bb4ea50a1ed76f5a853b8fb204640afb
noeyah2 <hy0708@cau.ac.kr>
2023-11-30 17:00:07 +0900

    TodoListRepositoryClassTest 테스트케이스 작성 중

--- src/test/java/com/project/todotodo/repository/TodoListRepositoryClassTest.java
+++ src/test/java/com/project/todotodo/repository/TodoListRepositoryClassTest.java
@@ -2,7 +2,6 @@ package com.project.todotodo.repository;
 
 import com.project.todotodo.model.Node;
 import com.project.todotodo.model.ToDoList;
-import com.project.todotodo.repository.TodoListRepositoryClass;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -36,6 +35,11 @@ class TodoListRepositoryClassTest {
 
         // Then
         // Add assertions to check if the removal is done as expected
+        verify(jdbcTemplate, times(1)).update(eq("DELETE FROM todo_lists WHERE node_id = ?"), eq(1L));
+        verify(jdbcTemplate, times(1)).update(eq("SELECT node_list_id FROM nodes WHERE parent_id = ?"), eq(1L));
+        verify(jdbcTemplate, times(1)).update(eq("DELETE FROM nodes WHERE node_id = ?"), eq(1L));
+        verify(jdbcTemplate, times(1)).update(eq("DELETE FROM node_lists WHERE node_list_id = ?"), anyLong());
+        verify(jdbcTemplate, times(1)).update(eq("DELETE FROM node_array_list WHERE node_id = ?"), eq(1L));
     }
 
     @Test
@@ -56,6 +60,16 @@ class TodoListRepositoryClassTest {
         // Then
         assertNotNull(ids);
         assertEquals(2, ids.size());
-        // Add more assertions to check if the creation is done as expected
+
+        // 생성이 예상대로 이루어졌는지 확인하는 추가적인 assert 구문을 추가하세요.
+
+        // longValue()를 호출하기 전에 null 체크를 추가합니다.
+        Long nodeId = ids.get(1);
+        assertNotNull(nodeId, "Node ID가 null이 아니어야 합니다");
+        assertEquals(1L, nodeId.longValue(), "Node ID는 1L이어야 합니다");
+
+        Long todoId = ids.get(1);
+        assertNotNull(todoId, "Todo ID는 null이 아니어야 합니다");
+        assertEquals(1L, todoId.longValue(), "Todo ID는 1L이어야 합니다");
     }
 }
